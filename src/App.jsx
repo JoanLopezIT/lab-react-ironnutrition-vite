30141f4d087c722f96d2b914b6179d99e0481def
JoanLopezIT <joan.lopez.job@gmail.com>
2023-08-23 17:38:55 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
 import "./App.css";
+import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm";
+import { useState } from 'react';
+
+
 
 function App() {
+  const [currentFood, setCurrentFood] = useState(foodsJson)
+
+
+  function deleteFood(foodId){
+    // console.log(foodId)
+    const filteredFood = currentFood.filter((singleFood)=>{  
+      return singleFood.id !== foodId
+
+    })
+    setCurrentFood(filteredFood)
+console.log(filteredFood)
+  }
+
+
+
+  function createFood(foodIWantToAdd) {
+
+    const newArray = [foodIWantToAdd, ...currentFood]
+
+    setCurrentFood(newArray);
+}
+
+
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
-    </div>
+      <AddFoodForm createFood={createFood}  />
+      {currentFood.map(food => {
+                return <FoodBox key={food.id} food={food} deleteFood={deleteFood} />;
+            })}
+        </div>
   );
 }
 
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,62 @@
-// Your code here
+import { useState } from "react";
+
+function AddFoodForm(props){
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("");
+    const [calories, setCalories] = useState("");
+    const [servings, setServings] = useState("");
+
+
+    function resetInputs(){
+        setName("");
+        setImage("");
+        setCalories("");
+        setServings("");
+      }
+
+
+      const handleSubmit = (e) => {
+        // I don't want the page to reload
+        e.preventDefault();
+        // Do we have access to the information?? YESSSS
+        const newFood = {
+          name: name,
+          image: image,
+          calories: calories,
+          servings: servings
+        }
+    
+        console.log(newFood);
+ 
+        props.createFood(newFood);
+        resetInputs();
+      }
+
+
+
+    return (
+        <form id="foodForm">
+          <label htmlFor="name">Name: </label>
+          <input type="text" id="name" name="name" required onChange={(event) => setName(event.target.value)} value={name} /><br /><br />
+    
+          <label htmlFor="image">Image:</label>
+          <input type="text" id="image" name="image" required onChange={(event) => setImage(event.target.value)} value={image} /><br /><br />
+    
+          <label htmlFor="calories">Calories:</label>
+          <input type="number" id="calories" name="calories" required onChange={(event) => setCalories(event.target.value)} value={calories} /><br /><br />
+    
+          <label htmlFor="servings">Servings:</label>
+          <input type="number" id="servings" name="servings" required onChange={(event) => setServings(event.target.value)} value={servings} /><br /><br />
+    
+          <button onClick={(e) => handleSubmit(e)} type="submit">Create</button>
+        </form>
+      );
+    }
+
+    export default AddFoodForm
+    
+    
+    
+    
+    
+    
